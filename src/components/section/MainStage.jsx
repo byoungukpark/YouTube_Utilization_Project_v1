2d581c6acfc0f41d3d8ec03c67915176cd70f7a7
bu.k <pyy3347@gmail.com>
2024-09-11 17:16:01 +0900

    search 작업중

--- src/components/section/MainStage.jsx
+++ src/components/section/MainStage.jsx
@@ -3,6 +3,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
+import Search from './Search'
 
 const MainStage = (props) => {
   return (
@@ -19,6 +20,8 @@ const MainStage = (props) => {
       <div>
         <Header />
         <main id='main' role='main'>
+          <Search />
+          
           {props.children}
         </main>
         <Footer />
