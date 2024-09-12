7a705fdc987742945251431ed02989027a2ea4c4
bu.k <pyy3347@gmail.com>
2024-09-12 17:58:55 +0900

    추천 영상 alc 반응형 작업

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import MainStage from '../components/section/MainStage'
 
+import { todayText } from '../data/today'
+import { Link } from 'react-router-dom'
+
 const Today = () => {
   return (
     <MainStage
       title = '추천 영상'
       description='오늘의 추천 유튜브 영상입니다.'
     >
-      Today
+      <section id='today_page'>
+        <h2>오늘의 추천 영상 입니다.</h2>
+
+        {todayText.map((today, key) => (
+          <div className='today__inner' key={key}>
+            <div className='today_thumb play__icon'>
+              <Link to={today.page}>
+                <img src={today.img} alt={today.title} />
+              </Link>
+            </div>
+            <div className='today__text'>
+              <span className='today'>today!</span>
+              <h3 className='title'>
+                <Link to={today.page}>
+                  {today.title}
+                </Link>
+              </h3>
+              <p className='desc'>{today.desc}</p>
+              <div className='info'>
+                <span id='author'>
+                  <Link to={`/channel/${today.channelId}`}>
+                    {today.author}
+                  </Link>
+                </span>
+                <span id='date'>{today.date}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+
+      </section>
     </MainStage>
   )
 }
