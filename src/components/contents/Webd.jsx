1ab0b725000c164a89d894dd7df006a293d42791
bu.k <pyy3347@gmail.com>
2024-09-14 23:22:59 +0900

    메인 섹션 나머지 콘텐츠 작업

--- src/components/contents/Webd.jsx
+++ src/components/contents/Webd.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import { webdText } from '../../data/webd'
+import { Link } from 'react-router-dom'
+
 const Webd = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='webd'>
+      <h2> 따라하는 코딩 추천 유튜브 영상 </h2>
+      <div className='video__inner'>
+        {webdText.map((video, key) => (
+          <div className='video' key={key}>
+            <div className='video__thumb play__icon'>
+              <Link to={`video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- src/components/contents/Website.jsx
+++ src/components/contents/Website.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import { websiteText } from '../../data/website'
+import { Link } from 'react-router-dom'
+
 const Website = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='website'>
+      <h2> 19컴공 개발 블로그를 소개합니다 </h2>
+      <div className='video__inner'>
+        {websiteText.map((video, key) => (
+          <div className='video' key={key}>
+            <div className='video__thumb play__icon'>
+              <Link to={`video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
