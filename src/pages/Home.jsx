3056300d76b0f9e1c1de9520de76bee7bf561d45
bu.k <pyy3347@gmail.com>
2024-09-15 16:19:03 +0900

    비디오 컴퍼넌트 통합 작업 및 Swiper 이미지 슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import MainStage from '../components/section/MainStage'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Gsap from '../components/contents/Gsap'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/videos/VideoSlider'
+
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
+import { developerText } from '../data/developer';
+import { todayText } from '../data/today'
 
 const Home = () => {
   return (
@@ -14,13 +18,13 @@ const Home = () => {
       title='19컴공 유튜브 개발일지'
       description='19컴공 유튜브 개발일지에 오신것을 환영합니다'
     >
-      <Today />
-      <Developer />
-      <Webd />
-      <Website />
-      <Gsap />
-      <Portfolio />
-      <Youtube />
+      <Today videos={todayText} id="today"/>
+      <Developer videos={developerText} title="😪 추천 개발자를 소개합니다." id="developer" />
+      <VideoSlider videos={webdText} title="😮 따라하는 코딩 추천 유튜브 영상" id="webd" />
+      <VideoSlider videos={websiteText} title="😛 19컴공 개발 블로그를 소개합니다" id="website" />
+      <VideoSlider videos={gsapText} title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!" id="gsap" />
+      <VideoSlider videos={portfolioText} title="🤗 포트폴리오 만드는 방법을 공유합니다." id="portfolio" />
+      <VideoSlider videos={youtubeText} title="😱 지금 이 코딩을 영상으로" id="youtube" />
     </MainStage>
   )
 }
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -40,7 +40,6 @@ const Today = () => {
             </div>
           </div>
         ))}
-
       </section>
     </MainStage>
   )
